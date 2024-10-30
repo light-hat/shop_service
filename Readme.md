3ec1889788cf11308446b03991d90b8bd96d1efc
l1ghth4t <pirogov30002@gmail.com>
2024-10-30 17:08:26 +0300

    update: Readme.md and configure linters

--- Readme.md
+++ Readme.md
@@ -1,2 +1,71 @@
 # Сервис магазинов
 
+## API
+
+![Swagger](assets/swagger.png)
+
+API задокументирован при помощи Swagger `drf-spectacular`.
+
+Тестирование API: `http://127.0.0.1/swagger/`
+
+Документация: `http://127.0.0.1/redoc`
+
+JSON: `http://127.0.0.1/.../`
+
+## База данных
+
+![ERD](assets/database.png)
+
+Связи между сущностями:
+
+- Улица к городу: `многие к одному`;
+
+- Магазин к улице: `многие к одному`;
+
+- Магазин к городу: `многие к одному`.
+
+СУБД: `PostgreSQL 15 версии`.
+
+ORM: `Django ORM`.
+
+## Конфигурирование
+
+
+
+## Деплой
+
+### 1. Ручной вариант
+
+
+
+Сервис будет доступен на `localhost:8000`.
+
+### 2. Docker
+
+> [!WARNING]  
+> В системе должен быть установлен Docker и Docker Compose.
+
+```bash
+docker-compose up -d --build
+```
+
+Сервис будет доступен на `localhost:80`.
+
+### 3. Vagrant
+
+> [!WARNING]  
+> Должен быть установлен Vagrant и VirtualBox.
+
+> [!TIP]
+> Базовый box можно скачать [отсюда](https://portal.cloud.hashicorp.com/vagrant/discover/ubuntu/focal64).
+
+```bash
+vagrant up
+```
+
+Сервис будет доступен на `localhost:80`.
+
+## Управление проектом через Make
+
+> [!NOTE]  
+> Актуально для вариантов деплоя 2 и 3.
--- service/config/settings.py
+++ service/config/settings.py
@@ -3,9 +3,9 @@
 """
 
 import os
-from django.core.management.utils import get_random_secret_key
 from os import environ
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -13,9 +13,9 @@ SECRET_KEY = get_random_secret_key()
 
 DEBUG = False
 
-HOST = environ.get('API_URL')
+HOST = environ.get("API_URL")
 
-PORT = environ.get('API_PORT')
+PORT = environ.get("API_PORT")
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -25,90 +25,90 @@ ALLOWED_HOSTS = [
 ]
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'api',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "api",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8000',
-    f'http://{HOST}:{PORT}',
+    "http://127.0.0.1:8000",
+    f"http://{HOST}:{PORT}",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': environ.get('POSTGRES_DB'),
-        'USER': environ.get('POSTGRES_USER'),
-        'PASSWORD': environ.get('POSTGRES_PASSWORD'),
-        'HOST': environ.get('POSTGRES_HOST'),
-        'PORT': environ.get('POSTGRES_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": environ.get("POSTGRES_DB"),
+        "USER": environ.get("POSTGRES_USER"),
+        "PASSWORD": environ.get("POSTGRES_PASSWORD"),
+        "HOST": environ.get("POSTGRES_HOST"),
+        "PORT": environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "static/"
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 STATIC_ROOT = STATIC_DIR
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
